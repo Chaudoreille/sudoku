cc6b25d78265f9821966f5e6a470398773f6c250
chaudo_a <a.chaudoreille@gmail.com>
2020-01-08 20:22:36 -0700

    import grid from file feature with 2 grid files (easy and hard)

--- sudoku/Sudoku.py
+++ sudoku/Sudoku.py
@@ -20,15 +20,115 @@ class Sudoku:
         return True
 
     @staticmethod
-    def CreateGridFromFile(file):
-        pass
+    def GridsFromFile(fileName):
+        """ takes a filename in parameter and \
+        extracts all the sudoku grids from the file. \
+        The file must contain one grid per line.\
+        Each grid must be a direct sequence of 49 numbers (0 means empty)"""
+        grids = []
+        file = open(fileName, 'r')
+        lines = file.readlines()
+
+        for line in lines:
+            grids.append(Sudoku.GridFromString(line))
+
+        return grids
+
+    @staticmethod
+    def GridFromString(str):
+        """ takes a string of 49 numbers (0 means empty) and \
+        builds a Sudoku grid from it """
+        grid = []
+
+        str = str.strip('\n')
+
+        for i in range(len(str)):
+            if (i % 9 == 0):
+                grid.append([])
+            grid[i//9].append(int(str[i]))
+
+        return grid
+
+    @staticmethod
+    def __build_border(thickness, position='middle'):
+        # private method to print grid borders in terminal for display
+        # only prints horizontal borders as vertical borders need to be embedded
+        # in horizontal lines
+        symbol = {
+        'thick' : {
+            'line' : '\u2501',
+            'top' : {
+                'left' : '\u250f',
+                'mid' : '\u2533',
+                'right' : '\u2513',
+                'separator' : '\u252f'
+            },
+            'middle' : {
+                'left' : '\u2523',
+                'mid' : '\u254b',
+                'right' : '\u252b',
+                'separator' : '\u253f'
+            },
+            'bottom' : {
+                'left' : '\u2517',
+                'mid' : '\u253b',
+                'right' : '\u251b',
+                'separator' : '\u2537'
+            }
+        },
+        'thin' : {
+            'line' : '\u2500',
+            'middle' : {
+                'left' : '\u2520',
+                'mid' : '\u2542',
+                'right' : '\u2528',
+                'separator' : '\u253c'
+            }
+        }
+        }
+        line = symbol[thickness]['line']
+        border = symbol[thickness][position]
+        square = (line * 3 + border['separator']) * 2 + line * 3
+
+        str = border['left'] + (square + border['mid']) * 2 + square
+        str += border['right'] + '\n'
+        return str
+
 
     def __init__(self, grid):
-        self.grid = grid if grid is not None else [[0] * 9] * 9
+        """grid must be a valid sudoku grid (9x9 list matrix of [0-9] numbers)\
+        see static methods to import from file"""
+        self.grid = grid if Sudoku.ValidGrid(grid) else [[0] * 9] * 9
 
+    def __str__(self):
+        """ Transforms grid in string. The string representation is made for\
+        display, and therefore builds a 9x9 table with unicode characters """
+        str = ''
+
+        for i, line in enumerate(self.grid):
+            if (i == 0):
+                str += Sudoku.__build_border('thick', 'top')
+            elif (i % 3 == 0):
+                str += Sudoku.__build_border('thick', 'middle')
+            else:
+                 str += Sudoku.__build_border('thin')
+            for j, number in enumerate(line):
+                str += '\u2503' if j % 3 == 0 else '\u2502'
+                str += ' ' + number.__str__() + ' ' if number != 0 else ' ' * 3
+            str += '\u2503\n'
+
+        str += Sudoku.__build_border('thick', 'bottom')
+
+        return str
 
     def SetGrid(self, grid):
-        """This method sets the sudoku grid to the new one \
+        """Sets the sudoku grid to the new one \
         if the provided parameter is a valid sudoku grid"""
         if (Sudoku.ValidGrid(grid)):
             self.grid = grid
+
+# grids = Sudoku.GridsFromFile("grids/easy_grids.txt")
+# for grid in grids:
+#     sudoku = Sudoku(grid)
+#     print(sudoku)
+    
--- sudoku/tests/test_sudoku.py
+++ sudoku/tests/test_sudoku.py
@@ -5,34 +5,38 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 from Sudoku import Sudoku
 
-def testGridValidator():
-    grid1 = [[1] * 9] * 9
-    grid2 = [[2] * 9] * 9
-    grid3 = [
-    [0,1,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,3,0,0,0,3,0,8],
-    [0,0,0,0,0,0,0,7,0],
-    [0,0,0,9,0,0,0,0,0],
-    [0,0,6,0,2,0,0,0,0],
-    [0,0,0,0,0,9,0,0,0],
-    [0,0,4,0,0,0,0,8,0],
-    [0,1,0,0,6,0,0,0,0],
-    ]
-    grid4 = [
-    [0,1,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,'a',3,0,0,0,3,0,8],
-    [0,0,0,0,0,0,0,7,0],
-    [0,0,0,9,0,0,0,0,0],
-    [0,0,6,0,2,0,0,0,0],
-    [0,0,0,0,0,9,0,0,0],
-    [0,0,4,0,0,0,0,8,0],
-    [0,1,0,0,6,0,0,0,0],
-    ]
-    grid5 = [[2] * 9] * 8
-    grid6 = [[2] * 8] * 9
+# valid grids
+grid1 = [[1] * 9] * 9
+grid2 = [[2] * 9] * 9
+grid3 = [
+[0,1,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,0,3,0,0,0,3,0,8],
+[0,0,0,0,0,0,0,7,0],
+[0,0,0,9,0,0,0,0,0],
+[0,0,6,0,2,0,0,0,0],
+[0,0,0,0,0,9,0,0,0],
+[0,0,4,0,0,0,0,8,0],
+[0,1,0,0,6,0,0,0,0],
+]
 
+# invalid grids
+grid4 = [
+[0,1,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0],
+[0,'a',3,0,0,0,3,0,8],
+[0,0,0,0,0,0,0,7,0],
+[0,0,0,9,0,0,0,0,0],
+[0,0,6,0,2,0,0,0,0],
+[0,0,0,0,0,9,0,0,0],
+[0,0,4,0,0,0,0,8,0],
+[0,1,0,0,6,0,0,0,0],
+]
+grid5 = [[2] * 9] * 8
+grid6 = [[2] * 8] * 9
+
+def testGridValidator():
+    # tests grid validity with different sets of grids
     assert(Sudoku.ValidGrid(grid1))
     assert(Sudoku.ValidGrid(grid2))
     assert(Sudoku.ValidGrid(grid3))
@@ -41,4 +45,29 @@ def testGridValidator():
     assert(not Sudoku.ValidGrid(grid6))
     assert(not Sudoku.ValidGrid(4))
 
+def testSetGrid():
+    pass
+
+def testGridFromString():
+    # tests importing a grid fromm a string of 49 characters
+    # Sudoku.GridFromString('000014000030000200070000000000900030601000000000000080200000104000050600000708000')
+    # Sudoku.GridFromString('000000520080400000030009000501000600200700000000300000600010000000000704000000030')
+    # Sudoku.GridFromString('602050000000003040000000000430008000010000200000000700500270000000000081000600000')
+    # Sudoku.GridFromString('052400000000070100000000000000802000300000600090500000106030000000000089700000000')
+    # Sudoku.GridFromString('602050000000004030000000000430008000010000200000000700500270000000000081000600000')
+    # Sudoku.GridFromString('092300000000080100000000000107040000000000065800000000060502000400000700000900000')
+    # Sudoku.GridFromString('600302000050000010000000000702600000000000054300000000080150000000040200000000700')
+    # Sudoku.GridFromString('060501090100090053900007000040800070000000508081705030000050200000000000076008000')
+    # Sudoku.GridFromString('005000987040050001007000000200048000090100000600200000300600200000009070000000500')
+    # Sudoku.GridFromString('306070000000000051800000000010405000700000600000200000020000040000080300000500000')
+    pass
+    
+def testGridFromFile():
+    # tests importing grids fromm file
+    grids = Sudoku.GridsFromFile("grids/easy_grids.txt")
+
+    for grid in grids:
+        assert(Sudoku.ValidGrid(grid))
+
 testGridValidator()
+testGridFromFile()
